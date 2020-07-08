857be4e5229f892931e8d60dbb343e61c6a997dc
Dzozew <jex.kara@gmail.com>
2020-07-08 13:41:08 +0200

    add files

--- company.py
+++ company.py
@@ -1,15 +1,25 @@
 class Company_next_move():
     """Creating company move"""
 
-    def __init__(self, first_buy_value, first_buy_pcs,
-                 first_buy_provision, buy_pack_value):
+    def __init__(self, first_buy_value_in_grosze, first_buy_pcs,
+                 first_buy_provision_in_grosze, buy_pack_value_in_grosze):
         """Initialization of atributes"""
-        self.first_buy_value = first_buy_value
+        self.first_buy_value_in_grosze = first_buy_value_in_grosze
         self.first_buy_pcs = first_buy_pcs
-        self.first_buy_provision = first_buy_provision
-        self.buy_pack = buy_pack_value
+        self.first_buy_provision_in_grosze = first_buy_provision_in_grosze
+        self.buy_pack_in_grosze = buy_pack_value_in_grosze
 
     def next_buy(self, drop_percentages=0):
         """Calculation of next buy value acording first buy value"""
-        next_buy_value = self.first_buy_value * (1 - (drop_percentages/100))
-        print(next_buy_value)
+        next_buy_value = (self.first_buy_value_in_grosze *
+                          (1 - (drop_percentages/100)))/100
+        print(
+            f"Kurs po jakim powinienem kupić kolejny pakiet to: "
+            f"{round(next_buy_value, 2)}")
+
+    def sell_set(self, up_percentages=0):
+        """Calculation of sell value"""
+        value_set = ((self.first_buy_value_in_grosze +
+                      self.first_buy_provision_in_grosze) *
+                      (1 + (up_percentages / 100)))/100
+        print(value_set)
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from company import Company_next_move
 
-company_1 = Company_next_move(50,25,3,1000)
+company_1 = Company_next_move(5000, 20, 300, 1000)
 
-company_1.next_buy(10)
+# for x in range(5, 100, 5):
+#    company_1.next_buy(x)
+
+company_1.sell_set(10)
